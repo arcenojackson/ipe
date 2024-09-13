de894c8c4631d3c87b0b9d33e8b36c59723f86f1
JacksonFA <jackson.f205@gmail.com>
2024-09-13 10:35:48 -0300

    chore(users): add log to users query

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   try {
     const { rows } = await db.sql`SELECT id, name, email FROM users`
+    console.warn('rows', rows)
     return NextResponse.json({ message: 'Lista de usuários.', data: rows }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ message: 'Erro interno.' }, { status: 500 })
