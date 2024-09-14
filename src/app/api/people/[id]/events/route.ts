f8d8dd2f6295e73b26e1640bb880af72f5bf546e
JacksonFA <jackson.f205@gmail.com>
2024-09-14 13:42:01 -0300

    fix(events): fix API to get events by logged user

--- src/app/api/people/[id]/events/route.ts
+++ src/app/api/people/[id]/events/route.ts
@@ -12,7 +12,7 @@ export async function GET(_: Request, { params }: RouteProps) {
       SELECT events.id, events.name, events.date, events.start, events_contents.people
       FROM events INNER JOIN events_contents ON (events.id = events_contents.event_id)
     `
-    const data = rows.filter((row) => row.people.includes(id))
+    const data = rows?.filter((row) => row?.people?.includes(id))
     return NextResponse.json({ message: 'Eventos do usuários', data }, { status: 200 })
   } catch (error) {
     console.log(error)
