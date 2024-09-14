0a3bd66bd3909737afdbed3b97ae051d63f9e6ca
JacksonFA <jackson.f205@gmail.com>
2024-09-13 22:30:18 -0300

    chore(users): add logs and update sw config

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,14 @@ import withPWA from 'next-pwa'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development'
-  }
+  // compiler: {
+  //   removeConsole: process.env.NODE_ENV !== 'development'
+  // }
 };
 
 export default withPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true
 })(nextConfig);
--- src/app/api/people/route.ts
+++ src/app/api/people/route.ts
@@ -6,8 +6,12 @@ export async function GET() {
     const { rows } = await db.sql`
       SELECT id, name, email FROM people ORDER BY name ASC;
     `
+    console.warn('rows')
+    console.warn(rows)
     return NextResponse.json({ message: 'OK', data: rows }, { status: 200 })
   } catch (error) {
+    console.error('error')
+    console.error(error)
     return NextResponse.json({ message: 'Erro interno.' }, { status: 500 })
   }
 }
